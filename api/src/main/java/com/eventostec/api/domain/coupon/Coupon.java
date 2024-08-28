fa806edf01526dc5d72a42016c2cc92ef7dd0d56
Caio Malvezzi <malvezzi.dev@gmail.com>
2024-08-27 21:22:03 -0300

    Adding repositories

--- api/src/main/java/com/eventostec/api/domain/coupon/Coupon.java
+++ api/src/main/java/com/eventostec/api/domain/coupon/Coupon.java
@@ -3,9 +3,13 @@ package com.eventostec.api.domain.coupon;
 import java.util.Date;
 import java.util.UUID;
 
+import com.eventostec.api.domain.event.Event;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,4 +30,8 @@ public class Coupon {
 	private String code;
 	private Integer discount;
 	private Date valid;
+	
+	@ManyToOne
+	@JoinColumn(name = "event_id")
+	private Event event;
 }
