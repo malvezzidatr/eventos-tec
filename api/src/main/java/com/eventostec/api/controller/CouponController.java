ca916573166055fd04b72a82908e852ec553ba6c
Caio Malvezzi <malvezzi.dev@gmail.com>
2024-08-29 10:43:36 -0300

    Coupon controller, service and DTO

---
+++ api/src/main/java/com/eventostec/api/controller/CouponController.java
@@ -0,0 +1,31 @@
+package com.eventostec.api.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eventostec.api.domain.coupon.Coupon;
+import com.eventostec.api.domain.coupon.CouponRequestDTO;
+import com.eventostec.api.services.CouponService;
+
+
+@RestController
+@RequestMapping("/api/coupon")
+public class CouponController {
+    @Autowired
+    private CouponService couponService;
+
+    @PostMapping("/event/{eventId}")
+    public ResponseEntity<Coupon> addCouponToEvent(@PathVariable UUID eventId, @RequestBody CouponRequestDTO couponRequestDTO) {
+        Coupon coupon = couponService.addCouponToEvent(eventId, couponRequestDTO);
+        
+        return ResponseEntity.ok(coupon);
+    }
+    
+}
---
+++ api/src/main/resources/db/migration/V4__modify-event-table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE event RENAME COLUMN "imgUrl" TO img_url;
+ALTER TABLE event RENAME COLUMN "eventUrl" TO event_url;
